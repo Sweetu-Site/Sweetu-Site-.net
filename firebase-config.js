e0123068f7dad306c14300b0e1e12abf638d6e6f
Sweetu-Site <sweetujee1908@gmail.com>
2026-04-08 23:38:49 +0500

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAA6KHZgYFAoikNhspJWIm5pukFpPAgGdc",  
+  authDomain: "sweetu-sites.firebaseapp.com",
+  projectId: "sweetu-sites",
+  storageBucket: "sweetu-sites.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
+
+export default firebaseConfig;
